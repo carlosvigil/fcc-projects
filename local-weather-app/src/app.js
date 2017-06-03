94fe1efd8a8e04b258f279b7959b21359781cf11
Carlos Vigil <carlos.vigil@icloud.com>
2017-06-03 01:25:54 -0400

    weather-app: get app working, functional paradigming

--- local-weather-app/src/app.js
+++ local-weather-app/src/app.js
@@ -1,15 +1,22 @@
 /* eslint-env browser */
-import { getWeatherData, urlBuilder } from './api.js'
-import { checkNavigator } from './browser.js'
+import { getWeatherData, urlBuilder, dummyData } from './api.js'
+import { checkNavigator, writeToDoc } from './browser.js'
 
 window.addEventListener('load', async function loaded () {
   console.log('DOCUMENT IS READY')
-  try {
-    window.sessionStorage.saveMeCalls = await checkNavigator()
-        .then(pos => urlBuilder(pos))
-        .then(url => getWeatherData(url))
-    // writeToDoc(window.sessionStorage.saveMeCalls)
-  } catch (error) { console.log(error) }
+  let weather = window.sessionStorage.saveMeCalls || false
+
+  if (!weather) {
+    try {
+      window.sessionStorage.saveMeCalls = await checkNavigator()
+          .then(pos => getWeatherData(urlBuilder(pos)))
+      weather = JSON.parse(window.sessionStorage.saveMeCalls)
+    } catch (error) {
+      console.log(error)
+      weather = dummyData
+    }
+    writeToDoc(weather)
+  } else writeToDoc(JSON.parse(weather))
 })
 
 // TODO: if no Nav rej Promise.all and use dummy data
