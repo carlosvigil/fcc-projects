d84312ea51f6d8e602ed368dd5c050da8bf66b9b
Carlos Vigil <carlos.vigil@icloud.com>
2017-06-02 23:30:57 -0400

    weather-app: learn async, promises, chaining

--- local-weather-app/src/api.js
+++ local-weather-app/src/api.js
@@ -1,46 +1,52 @@
 /* eslint-env browser */
-import { lang, weather, writeToDoc } from './main.js'
 
-const key = '767b3baa2aca876fa6ea5e4fbd75228c'
-const request = new XMLHttpRequest()
-const apiLangs = [
-  'ar', 'az', 'be', 'bg', 'bs', 'ca', 'cs', 'de', 'el',
-  'en', 'es', 'et', 'fr', 'hr', 'hu', 'id', 'it', 'is', 'kw', 'nb', 'nl',
-  'pl', 'pt', 'ru', 'sk', 'sl', 'sr', 'sv', 'tet', 'tr', 'uk', 'x-pig-latin',
-  'zh', 'zh-tw'
-]
+// const apiLangs = [
+//   'ar', 'az', 'be', 'bg', 'bs', 'ca', 'cs', 'de', 'el',
+//   'en', 'es', 'et', 'fr', 'hr', 'hu', 'id', 'it', 'is', 'kw', 'nb', 'nl',
+//   'pl', 'pt', 'ru', 'sk', 'sl', 'sr', 'sv', 'tet', 'tr', 'uk', 'x-pig-latin',
+//   'zh', 'zh-tw'
+// ]
+// const weather = JSON.parse(window.sessionStorage.saveMeCalls) || false
+// let scaleChar = weather ? weather.flags.units : ''
 
+// NOTE: FUNCTIONAL CHECK 🆗
 // BUILD REQUEST URL
-function urlBuilder (position) {
+export function urlBuilder (position) {
+  const lang = 'en'
+  const key = '767b3baa2aca876fa6ea5e4fbd75228c'
   const lat = position.coords.latitude
   const lon = position.coords.longitude
   const cors = 'https://cors-anywhere.herokuapp.com/'
   const darkSky = 'https://api.darksky.net/forecast/'
   const queries = `?exclude=minutely,hourly,daily&lang=${lang}&units=auto`
   const url = `${cors}${darkSky}${key}/${lat},${lon}${queries}`
-  const obj = { lat, lon, url }
-  return obj
+  return url
 }
 
-// API REQUEST CALLBACKS
-request.onloadstart = _ => console.log('LOAD START')
-request.onprogress = _ => console.log('LOADING')
-// GET THE DATA AND STORE IT
-request.onload = function () { // TODO: return the parsed object
-  if (this.status >= 200 && this.status < 400) {
-    // store response
-    window.sessionStorage['saveMeCalls'] = this.response
-    // continue report
-    console.log('Dark Sky API Data:')
-    console.log(weather)
-  } else {
-    window.alert('An error occured on retrieving data.')
-  }
-}
-// WRITE DATA TO THE DOCUMENT
-request.onloadend = _ => {
-  console.log('LOAD END')
-  writeToDoc(weather)
+// NOTE: FUNCTIONAL CHECK 🆗
+// CALL API TO GET DATA
+export function getWeatherData (url) {
+  return new Promise(function promiseResponse (resolve, reject) {
+    const request = new XMLHttpRequest()
+    // REQUEST CALLBACKS
+    request.open('GET', url)
+    request.onloadstart = _ => console.log('LOAD START')
+    request.onprogress = _ => console.log('*')
+    request.onload = function requestOnload () {
+      console.log(`${request.status}: ${request.statusText}`)
+      if (request.status >= 200 && request.status < 400) {
+        resolve(request.response)
+      } else {
+        reject(Error(request.status, request.statusText))
+      }
+    }
+    request.onerror = _ => reject(Error('Network error on getting data.'))
+    request.onloadend = _ => console.log('LOAD END')
+    request.send()
+  })
 }
 
-export { urlBuilder, apiLangs }
+// CHECK TEMP SCALE
+// function tempScaleCheck () {
+//   weather.flags.units === 'us' ? scaleChar = 'F' : scaleChar = 'C'
+// }
--- local-weather-app/src/app.js
+++ local-weather-app/src/app.js
@@ -1,22 +1,15 @@
 /* eslint-env browser */
-import { weather, checkLanguage, checkNavigator, writeToDoc } from './main.js'
+import { getWeatherData, urlBuilder } from './api.js'
+import { checkNavigator } from './browser.js'
 
-const sky = new Skycons({'color': 'white'})
-
-document.addEventListener('DOMContentLoaded', function loaded () {
+window.addEventListener('load', async function loaded () {
   console.log('DOCUMENT IS READY')
-  sky.add('icon', Skycons.CLEAR_DAY)
-  sky.play()
-// GO!
-  ;(function start () {
-    console.log('START')
-    if (weather) {
-      console.log('Saving calls by using stored data.')
-      checkLanguage(weather)
-      writeToDoc(weather)
-    } else {
-      checkLanguage()
-      checkNavigator()
-    }
-  })()
+  try {
+    window.sessionStorage.saveMeCalls = await checkNavigator()
+        .then(pos => urlBuilder(pos))
+        .then(url => getWeatherData(url))
+    // writeToDoc(window.sessionStorage.saveMeCalls)
+  } catch (error) { console.log(error) }
 })
+
+// TODO: if no Nav rej Promise.all and use dummy data
