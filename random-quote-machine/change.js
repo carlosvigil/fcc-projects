7918d2bc0191ecf95542b8340ada8b58646b195b
Carlos Vigil <18217279+carlosvigil@users.noreply.github.com>
2023-05-26 15:59:50 -0400

    random-quote-machine

---
+++ random-quote-machine/change.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+  var book = [], chapter = 0;
+
+  $.getJSON('tao-te-ching.json', function(json) {
+    $.each(json, function(key, val) {
+      book.push(val);
+    });
+    $('article').html(book[chapter]);
+  });
+  console.log(book);
+
+  $('.random').on('click', function() {
+    chapter = Math.floor(Math.random() * (81 - 1)) + 1;
+
+    $('article').fadeOut(1364.000733092, function() {
+     $('article').html(book[chapter]);
+     $('body').animate({ scrollTop: 0 }, 521.0019193625);
+     $('article').fadeIn(842.998813731);
+  });
+
+    console.log('Current chapter: ' + (chapter + 1));
+    console.log(book[chapter]);
+  });
+});
