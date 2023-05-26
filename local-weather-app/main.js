745ca6aeda205838c9b207fd6b0bb2936d184e0d
Carlos Vigil <18217279+carlosvigil@users.noreply.github.com>
2023-05-26 16:00:22 -0400

    reach a satisfactory stage in api call and doc writing

--- local-weather-app/main.js
+++ local-weather-app/main.js
@@ -2,10 +2,11 @@
 /* eslint-env browser */
 
 // VARIABLES
+const select = (element) => document.querySelector(element)
 const geoOptions = {
   enableHighAccuracy: true,
-  timeout: 5000,
-  maximumAge: 900000
+  timeout: 15000,
+  maximumAge: Infinity
 }
 const key = '129f708692983c0e4c3192f214aaf598'
 const request = new XMLHttpRequest()
@@ -13,12 +14,12 @@ let weather
 
 // IS DOC READY ?
 ;(function main (start) {
-  if (document.readyState !== 'loading') {
+  if (document.readyState === 'complete') {
     console.log('DOCUMENT IS READY')
     start()
   } else {
     console.log('NOT READY, LISTENER & CALLBACK ADDED')
-    document.addEventListener('DOMContentLoaded', start)
+    window.addEventListener('load', () => start())
   }
 }(checkNavigator))
 
@@ -43,7 +44,7 @@ function geoSuccess (position) {
   let time = new Date(position.timestamp).toUTCString()
   const lat = position.coords.latitude
   const lon = position.coords.longitude
-  const url = `http://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/${key}/${lat},${lon}`
+  const url = `https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/${key}/${lat},${lon}`
   // CONSOLE REPORT
   console.log(`GEOLOCATION AVAILABLE
     Latest: ${time}
@@ -55,19 +56,28 @@ function geoSuccess (position) {
 }
 
 // API REQUEST CALLBACKS
-request.onloadstart = () => console.log('LOAD START')
-request.onprogress = () => console.log('LOADING')
+request.onloadstart = _ => console.log('LOAD START')
+request.onprogress = _ => console.log('LOADING')
 request.onload = function () {
   if (this.status >= 200 && this.status < 400) {
     // PARSE IT
     weather = JSON.parse(this.response)
+    // CONTINUE REPORT
     console.log('Dark Sky API Data:')
     console.log(weather)
   } else {
     window.alert('An error occured on retrieving data.')
   }
 }
-request.onloadend = () => console.log('LOAD END')
-request.onerror = function () {
-  console.log(this.response)
+request.onloadend = _ => {
+  console.log('LOAD END')
+  writeToDoc(weather)
+}
+
+// FINALLY, WRITE TO THE DOC
+function writeToDoc (weather) {
+  select('.location').innerHTML = `${weather.latitude}, ${weather.longitude}`
+  select('.one-liner').innerHTML = weather.currently.summary
+  // todo: logic for celsius or farenheit, checking api flags
+  select('.temp').innerHTML = weather.currently.temperature
 }
