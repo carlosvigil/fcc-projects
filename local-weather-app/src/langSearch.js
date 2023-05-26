81a6cc99265d31b91eb8c05c97dbc67421d70202
Carlos Vigil <18217279+carlosvigil@users.noreply.github.com>
2023-05-26 16:00:29 -0400

    weather: split binary search Use a fuzzy and exact match algo because the current implementation skips certain matches

--- local-weather-app/src/langSearch.js
+++ local-weather-app/src/langSearch.js
@@ -2,14 +2,43 @@
 
 // It might be faster to use a search other than linear, but no benchmarks for this yet
 function binarySearch (arr, val) {
+  let exactMatch = exact(arr, val)
+  let fuzzyMatch = fuzzy(arr, val)
+
+  // return search results
+  if (exact) {
+    return [2]
+  } else if (fuzzyMatch) {
+    return [1, fuzzyMatch]
+  } else return 0
+}
+
+function exact (arr, val) {
   let lowIndex = 0
   let highIndex = arr.length - 1
   let midIndex = Math.floor((lowIndex + highIndex) / 2)
-  let exact = arr[midIndex].indexOf(val) === 0
-  let fuzzyCheck = val.indexOf(arr[midIndex]) === 0
-  let fuzzyMatch
+  let match = arr[midIndex].indexOf(val) === 0
 
-  while (!exact && lowIndex < highIndex) {
+  while (!match && lowIndex < highIndex) {
+    // change the center to reflect the decided halve of the arr
+    if (val < arr[midIndex]) {
+      highIndex = midIndex - 1
+    } else if (val > arr[midIndex]) {
+      lowIndex = midIndex + 1
+    }
+    // these variables don't update unless reassigned
+    midIndex = Math.floor((lowIndex + highIndex) / 2)
+    match = arr[midIndex].indexOf(val) === 0
+  }
+}
+
+function fuzzy (arr, val) {
+  let lowIndex = 0
+  let highIndex = arr.length - 1
+  let midIndex = Math.floor((lowIndex + highIndex) / 2)
+  let match = val.indexOf(arr[midIndex]) === 0
+
+  while (!match && lowIndex < highIndex) {
     // BUG: May iterate through all fuzzy matches instead of keeping best match
     fuzzyMatch = fuzzyCheck ? arr[midIndex] : null
     // change the center to reflect the decided halve of the arr
@@ -20,16 +49,10 @@ function binarySearch (arr, val) {
     }
     // these variables don't update unless reassigned
     midIndex = Math.floor((lowIndex + highIndex) / 2)
-    exact = arr[midIndex].indexOf(val) === 0
-    fuzzyCheck = val.indexOf(arr[midIndex]) === 0
+    match = val.indexOf(arr[midIndex]) === 0
   }
-  // return search results
-  if (exact) {
-    return [2]
-  } else if (fuzzyMatch) {
-    return [1, fuzzyMatch]
-  } else return 0
 }
+
 // to provide a better UX, send best matched language string to the calling f()
 export default function setLanguage (arr, lang) {
   const results = binarySearch(arr, lang)
