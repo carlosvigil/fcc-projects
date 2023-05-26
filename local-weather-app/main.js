50493f900874bde6b6f3cf45fce999f93ddf14c0
Carlos Vigil <18217279+carlosvigil@users.noreply.github.com>
2023-05-26 16:00:21 -0400

    add base api call for weather app - check the api docs for options later on

--- local-weather-app/main.js
+++ local-weather-app/main.js
@@ -1,6 +1,7 @@
 'use strict'
 /* eslint-env browser */
 
+// VARIABLES
 const geoOptions = {
   enableHighAccuracy: true,
   timeout: 5000,
@@ -8,22 +9,23 @@ const geoOptions = {
 }
 const key = '129f708692983c0e4c3192f214aaf598'
 const request = new XMLHttpRequest()
+let weather
 
 // IS DOC READY ?
-;((fn) => {
+;(function main (start) {
   if (document.readyState !== 'loading') {
     console.log('DOCUMENT IS READY')
-    fn()
+    start()
   } else {
     console.log('NOT READY, LISTENER & CALLBACK ADDED')
-    document.addEventListener('DOMContentLoaded', fn)
+    document.addEventListener('DOMContentLoaded', start)
   }
-})(checkNavigator())
+}(checkNavigator))
 
 // CHECK FOR LOCATION AVAILABILITY
 function checkNavigator () {
   if (navigator.geolocation) {
-    // YUP
+    // YUP, GO
     navigator.geolocation.getCurrentPosition(geoSuccess, geoFailure, geoOptions)
   } else {
     // NOPE
@@ -31,28 +33,41 @@ function checkNavigator () {
   }
 }
 
-// GEOLOCATION SUCCESS CALLBACK
-function geoSuccess (position) {
-  let time = new Date(position.timestamp).toUTCString()
-  const lat = position.coords.latitude
-  const lon = position.coords.longitude
-  const url = `https://api.darksky.net/forecast/${key}/${lat},${lon}`
-  // GET DATA
-  apiRequest(url)
-  // CONSOLE REPORT
-  console.log(`GEOLOCATION AVAILABLE
-    Latest: ${time}
-    Latitude: ${lat}
-    Longitude: ${lon}
-    Dark Sky Data: ${JSON.stringify(weather)}`)
-}
-
 // GEOLOCATION FAILURE CALLBACK
 function geoFailure (error) {
   console.log(`ERROR: ${error.message}; CODE: ${error.code}.`)
 }
 
-// GET JSON THINGS
-function apiRequest (url) {
+// GEOLOCATION SUCCESS CALLBACK
+function geoSuccess (position) {
+  let time = new Date(position.timestamp).toUTCString()
+  const lat = position.coords.latitude
+  const lon = position.coords.longitude
+  const url = `http://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/${key}/${lat},${lon}`
+  // CONSOLE REPORT
+  console.log(`GEOLOCATION AVAILABLE
+    Latest: ${time}
+    Latitude: ${lat}
+    Longitude: ${lon}`)
+  // GET DATA
   request.open('GET', url)
+  request.send()
+}
+
+// API REQUEST CALLBACKS
+request.onloadstart = () => console.log('LOAD START')
+request.onprogress = () => console.log('LOADING')
+request.onload = function () {
+  if (this.status >= 200 && this.status < 400) {
+    // PARSE IT
+    weather = JSON.parse(this.response)
+    console.log('Dark Sky API Data:')
+    console.log(weather)
+  } else {
+    window.alert('An error occured on retrieving data.')
+  }
+}
+request.onloadend = () => console.log('LOAD END')
+request.onerror = function () {
+  console.log(this.response)
 }
